afbe362ca4f8f1e9981fb6c04a2aba4e90388328
yjduoduo <wxjlmr@126.com>
2015-07-05 18:19:42 +0800

    debug临时文件

---
+++ build-ffmpegplayandroid-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_2-Debug/android-build/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\git\android_withQt\build-ffmpegplayandroid-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_2-Debug\android-build\bin\AndroidManifest.xml : \
+D:\git\android_withQt\build-ffmpegplayandroid-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_2-Debug\android-build\AndroidManifest.xml \
---
+++ build-ffmpegplayandroid-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_2-Debug/android-build/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
@@ -0,0 +1,3 @@
+D:\git\android_withQt\build-ffmpegplayandroid-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_2-Debug\android-build\gen\org\kde\necessitas\ministro\IMinistroCallback.java: \
+  D:\git\android_withQt\build-ffmpegplayandroid-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_2-Debug\android-build\src\org\kde\necessitas\ministro\IMinistroCallback.aidl 
+
